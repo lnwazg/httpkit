01a277a04de6dd554e10a227c2de71aabf5d9087
李楠 <lnwazg@126.com>
2018-09-14 18:03:29 +0800

    日志完善

--- src/main/java/com/lnwazg/httpkit/filter/common/CORSFilter.java
+++ src/main/java/com/lnwazg/httpkit/filter/common/CORSFilter.java
@@ -15,20 +15,6 @@ public class CORSFilter extends BaseController implements CtrlFilter
 {
     public void doFilter(CtrlFilterChain filterChain)
     {
-        //            server.addExtraResponseHeaders(Maps.asStrMap("Access-Control-Allow-Origin", "*"));
-        
-        //例如： 当前所处的页面
-        //            http://insurance.cekid.com/order/form/132/1273
-        //想要访问的json请求的地址
-        //            Request URL:http://baoxian.cekid.com/front_insurance/orderInte/queryRecognizeeList.do?skuId=1273
-        
-        //请求头中：
-        //            Origin:http://insurance.cekid.com
-        
-        //响应头中要做的配置：
-        //            Access-Control-Allow-Credentials:true
-        //            Access-Control-Allow-Origin:http://insurance.cekid.com
-        
         addHeaderPre("Access-Control-Allow-Credentials", true);
         String origin = getHeader("Origin");
         if (StringUtils.isNotEmpty(origin))
